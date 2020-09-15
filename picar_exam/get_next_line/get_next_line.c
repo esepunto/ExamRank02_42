8ca47175525db12e2c217f16009bafc7ae643a7b
Samuel Sacristan sanz <ssacrist@c2r9s3.42madrid.com>
2020-09-15 18:57:24 +0200

    GNL: to join 2 functions to 1

--- picar_exam/get_next_line/get_next_line.c
+++ picar_exam/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ssacrist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/02 12:53:42 by ssacrist          #+#    #+#             */
-/*   Updated: 2020/09/15 08:44:53 by ssacrist         ###   ########.fr       */
+/*   Updated: 2020/09/15 18:53:31 by ssacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,34 @@ size_t	ft_strlen(const char *str)
 	return (c);
 }
 
-char	*ft_strdup(const char *str)
-{
-	char	*twin;
-	int		i;
-
-	if (!(twin = malloc(sizeof(char) * (ft_strlen(str) + 1))))
-		return (NULL);
-	i = 0;
-	while (*str != '\0')
-		twin[i++] = *str++;
-	twin[i] = '\0';
-	return (twin);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_b4chr(char const *s, char chr)
 {
 	char	*dest;
 	int		i;
 
 	i = 0;
-	if (!(dest = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+	if (!(dest = malloc(sizeof(char) * (ft_strlen(s) + 1))))
+		return (NULL);
+	while (*s != chr)
+		dest[i++] = *s++;
+	dest[i++] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*join;
+	int		i;
+
+	i = 0;
+	if (!(join = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
 			return (NULL);
 	while (*s1 != '\0')
-		dest[i++] = *s1++;
+		join[i++] = *s1++;
 	while (*s2 != '\0')
-		dest[i++] = *s2++;
-	dest[i] = '\0';
-	return (dest);
+		join[i++] = *s2++;
+	join[i] = '\0';
+	return (join);
 }
 
 char	*ft_strchr(const char *str, int char2find)
@@ -63,37 +63,23 @@ char	*ft_strchr(const char *str, int char2find)
 	return (0);
 }
 
-char	*ft_b4eol(char *wr_nd_wipe)
-{
-	int		i;
-	char	*b4eol;
-
-	i = 0;
-	if (!(b4eol = malloc(ft_strlen(wr_nd_wipe) + 1)))
-		return (NULL);
-	while (*wr_nd_wipe != '\n')
-		b4eol[i++] = *wr_nd_wipe++;
-	b4eol[i++] = '\0';
-	return (b4eol);
-}
-
 int		ft_chop(char **wr_nd_wipe, char **line)
 {
 	char	*aftr_eol;
 
 	if (*wr_nd_wipe && ft_strchr(*wr_nd_wipe, '\n') > 0)
 	{
-		*line = ft_b4eol(*wr_nd_wipe);
+		*line = ft_b4chr(*wr_nd_wipe, '\n');
 		aftr_eol = ft_strchr(*wr_nd_wipe, '\n') + 1;
-		aftr_eol = ft_strdup(aftr_eol);
+		aftr_eol = ft_b4chr(aftr_eol, '\0');
 		free(*wr_nd_wipe);
 		*wr_nd_wipe = aftr_eol;
 		return (1);
 	}
 	else if (*wr_nd_wipe != 0)
-		*line = ft_strdup(*wr_nd_wipe);
+		*line = ft_b4chr(*wr_nd_wipe, '\0');
 	else
-		*line = ft_strdup("");
+		*line = ft_b4chr("", '\0');
 	free(*wr_nd_wipe);
 	return (0);
 }
@@ -112,7 +98,7 @@ int		get_next_line(char **line)
 	{
 		buffer[bytes_buf] = '\0';
 		if (!wr_nd_wipe)
-			wr_nd_wipe = ft_strdup(buffer);
+			wr_nd_wipe = ft_b4chr(buffer, '\0');
 		else
 		{
 			aux = ft_strjoin(wr_nd_wipe, buffer);
