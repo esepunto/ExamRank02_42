85dd0861f63437ee0c51364f4a1c8f10c908470a
Samuel Sacristan sanz <ssacrist@c2r7s6.42madrid.com>
2020-09-16 11:36:00 +0200

    WTF! Not need buffer_size

--- picar_exam/get_next_line/get_next_line.h
+++ picar_exam/get_next_line/get_next_line.h
@@ -6,17 +6,12 @@
 /*   By: ssacrist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/02 13:24:04 by ssacrist          #+#    #+#             */
-/*   Updated: 2020/09/15 22:03:41 by ssacrist         ###   ########.fr       */
+/*   Updated: 2020/09/16 00:16:43 by ssacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 32
-
-# endif
 
 # include <stdlib.h>
 # include <unistd.h>
