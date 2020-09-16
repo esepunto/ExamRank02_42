d5a85da24012014d801a7f667c72698c649166b1
Samuel Sacristan sanz <ssacrist@c2r7s6.42madrid.com>
2020-09-17 01:21:12 +0200

    GNL more efficient

--- picar_exam/get_next_line/get_next_line.c
+++ picar_exam/get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ssacrist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/02 12:53:42 by ssacrist          #+#    #+#             */
-/*   Updated: 2020/09/17 00:53:41 by ssacrist         ###   ########.fr       */
+/*   Updated: 2020/09/17 01:20:35 by ssacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,26 @@ char	*ft_strjoin(char *line, char *buffer)
 		join[i] = line[i];
 		i++;
 	}
-	join[i++] = buffer[0];
-	join[i++] = '\0';
 	free(line);
+	join[i++] = buffer[0];
+	join[i++] = buffer[1];
 	return (join);
 }
 
 int		get_next_line(char **line)
 {
 	int			bytes_buf;
-	char		*buffer;
+	char		buffer[2];
 
-	if (!(*line = malloc(1)) || !line || !(buffer = malloc(2)))
+	if (!(*line = malloc(1)) || !line)
 		return (-1);
 	(*line)[0] = '\0';
 	while ((bytes_buf = read(0, buffer, 1)) > 0)
 	{
 		buffer[1] = '\0';
-		if (buffer[0] == '\n' || buffer[0] == '\0')
-		{	
+		if (buffer[0] == '\0' || buffer[0] == '\n')
 			break;
-		}
 		*line = ft_strjoin(*line, buffer);
 	}
-	free(buffer);
 	return (bytes_buf);
 }
