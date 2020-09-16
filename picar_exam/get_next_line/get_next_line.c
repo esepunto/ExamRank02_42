1a6af15324ffc4a7358178f21a84a31377b373fc
Samuel Sacristan sanz <ssacrist@c2r7s6.42madrid.com>
2020-09-16 18:50:50 +0200

    To recode gnl without buffer_size

--- picar_exam/get_next_line/get_next_line.c
+++ picar_exam/get_next_line/get_next_line.c
@@ -6,105 +6,46 @@
 /*   By: ssacrist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/02 12:53:42 by ssacrist          #+#    #+#             */
-/*   Updated: 2020/09/16 11:38:30 by ssacrist         ###   ########.fr       */
+/*   Updated: 2020/09/16 18:32:41 by ssacrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static size_t	ft_strlen(const char *str)
-{
-	size_t	c;
-
-	c = 0;
-	while (str[c])
-		c++;
-	return (c);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char buffer, char *wr_nd_wipe)
 {
 	char	*join;
 	int		i;
 
-	i = 0;
-	if (!(join = malloc(ft_strlen(s1) + ft_strlen(s2) + 1)))
+	i = 1;
+	if (!(join = malloc(3 + sizeof(*))))
 		return (NULL);
-	while (*s1 != '\0')
-		join[i++] = *s1++;
-	while (*s2 != '\0')
-		join[i++] = *s2++;
+	join[0] = buffer;
+	while (**line != '\0')
+		join[i++] = **line++;
 	join[i] = '\0';
 	return (join);
-}
-
-char	*ft_strchr(const char *str, int chr2find)
-{
-	while (*str)
-	{
-		if (*str == chr2find)
-			return ((char*)str);
-		str++;
-	}
-	return (0);
-}
-
-char	*ft_b4chr(char const *s, char chr)
-{
-	char	*b4chr;
-	int		i;
-
-	i = 0;
-	if (!(b4chr = malloc(ft_strlen(s) + 1)))
-		return (NULL);
-	while (*s != chr)
-		b4chr[i++] = *s++;
-	b4chr[i++] = '\0';
-	return (b4chr);
-}
-
-int		ft_chop(char **wr_nd_wipe, char **line)
-{
-	char	*aftr_eol;
-
-	if (ft_strchr(*wr_nd_wipe, '\n'))
-	{
-		*line = ft_b4chr(*wr_nd_wipe, '\n');
-		aftr_eol = ft_strchr(*wr_nd_wipe, '\n') + 1;
-		aftr_eol = ft_b4chr(aftr_eol, '\0');
-		free(*wr_nd_wipe);
-		*wr_nd_wipe = aftr_eol;
-		return (1);
-	}
-	else if (*wr_nd_wipe != 0)
-		*line = ft_b4chr(*wr_nd_wipe, '\0');
-	else
-		*line = ft_b4chr("", '\0');
-	free(*wr_nd_wipe);
-	return (0);
 }
 
 int		get_next_line(char **line)
 {
 	int			bytes_buf;
-	char		*buffer;
+	char		buffer[1];
 	static char	*wr_nd_wipe;
-	char		*aux;
 
-	if (!(buffer = malloc(1 + 1))
-			|| !line ||read(0, buffer, 0) < 0)
+	if (!line || read(0, buffer, 0) < 0)
 		return (-1);
+	*line[0] = '\0';
 	while ((bytes_buf = read(0, buffer, 1)) > 0)
 	{
-		buffer[bytes_buf] = '\0';
-		if (!wr_nd_wipe)
-			wr_nd_wipe = ft_b4chr("", '\0');
-		aux = ft_strjoin(wr_nd_wipe, buffer);
-		free(wr_nd_wipe);
-		wr_nd_wipe = aux;
-		if (ft_strchr(wr_nd_wipe, '\n'))
-			break ;
+		if (buffer[0] == '\n')
+		{
+			line = wr_nd_wipe;
+
+			return (1);
+		else if (buffer[0] != '\0')
+			wr_nd_wipe = ft_strjoin(buffer[0], wr_nd_wipe);
 	}
 	free(buffer);
-	return (ft_chop(&wr_nd_wipe, line));
+	return (0);
 }
