719199191c51031553ff3c2e0972b59a6ab3290b
Samuel Sacristan sanz <ssacrist@c2r7s6.42madrid.com>
2020-09-17 03:03:52 +0200

    To del files with gnl's test

--- picar_exam/get_next_line/test.sh
+++ picar_exam/get_next_line/test.sh
@@ -6,7 +6,7 @@
 #    By: ssacrist </var/mail/ssacrist>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/08/10 09:30:55 by ssacrist          #+#    #+#              #
-#    Updated: 2020/09/16 21:20:23 by ssacrist         ###   ########.fr        #
+#    Updated: 2020/09/17 03:01:59 by ssacrist         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,4 +15,4 @@ gcc -Wall -Werror -Wextra -g get_next_line.c main.c -o get_next_line
 cat -e yours_.res > yours.res
 cat -e < get_next_line.c > original.res
 diff -y --suppress-common-line original.res yours.res
-#rm -rf original.res yours_.res yours.res get_next_line
+rm -rf original.res yours_.res yours.res get_next_line
